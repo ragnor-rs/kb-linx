e219204ace0b93b03288e93efa4632e28817f227
Renat <ragnor.rs@gmail.com>
2013-01-24 17:16:54 +0400

    added answer processing

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 /*
+ * DONE: question generation (http://localhost/getQuestion)
+ * DONE: relationship creation (http://localhost/submitAnswer?target=3&required=4)
+ * TODO: bot protection
  * TODO: i18n
- * TODO: add question answering method
  */
 
 var http = require("http");
 var neo4j = require("neo4j");
 var async = require("async");
+var url = require('url');
 
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
@@ -13,18 +16,11 @@ var db = new neo4j.GraphDatabase('http://localhost:7474');
  * Get total number of nodes
  */
 function getNodeCount(processCountFunc) {
-
-    var query = [
-        'START n=node(*)',
-        'RETURN count(n)'
-    ].join('\n');
-
-    db.query(query, {}, function (err, results) {
+    db.query('START n=node(*)\nRETURN count(n)', {}, function (err, results) {
         if (err) throw err;
         var n = results[0]["count(n)"];
         processCountFunc(n);
     });
-
 }
 
 /**
@@ -32,47 +28,44 @@ function getNodeCount(processCountFunc) {
  */
 var prevNodeIndex = -1;
 function getRndNodeIndex(numNodes) {
-
-    var rnd;
-
     while (true) {
-        rnd = Math.round(Math.random() * (numNodes - 1));
+        var rnd = Math.round(Math.random() * (numNodes - 1));
         if (rnd != prevNodeIndex) {
             prevNodeIndex = rnd;
             return rnd;
         }
     }
-
 }
 
-/**
- * Asynchronously returns a node by index
- */
-function getNodeByIndex(i, processNodeFunc) {
-
-    var query = [
-        'START n=node(*)',
-        'RETURN n',
-        'SKIP {rnd} LIMIT 1'
-    ].join('\n');
-
-    db.query(query, {"rnd": i}, function (err, results) {
-        if (err) throw err;
-        var n = results[0]['n'];
-        processNodeFunc(n);
-    });
-
-}
-
-function createGetNodeTask(nodeIndex) {
+function createGetNodeByIndexTask(nodeIndex) {
     return function(cb) {
-        getNodeByIndex(nodeIndex, function(n) {
+        db.query('START n=node(*)\nRETURN n\nSKIP {rnd} LIMIT 1', {"rnd": nodeIndex}, function (err, results) {
+            if (err) throw err;
+            var n = results[0]['n'];
             cb(null, n);
         });
     };
 }
 
-function generateResponse(nodeRequests, response) {
+function createGetNodeByIdTask(nodeId) {
+    return function(cb) {
+        db.getNodeById(nodeId, function (err, n) {
+            if (err) throw err;
+            cb(null, n);
+        });
+    };
+}
+
+function createSaveRelationshipTask(n1, n2) {
+    return function(cb) {
+        n1.createRelationshipFrom(n2, "is based on", {}, function (err, r) {
+            if (err) throw err;
+            cb(null, r);
+        });
+    };
+}
+
+function loadNodesAndQuestion (nodeRequests, response) {
     async.series(nodeRequests, function(err, results) {
 
         if (err) {
@@ -82,14 +75,13 @@ function generateResponse(nodeRequests, response) {
             return;
         }
 
-        /*
-         return question
-         */
+        var question = {
+            target: results[0].data,
+            required: new Array()
+        };
 
-        var question = {options: new Array()};
-
-        for (var i = 0; i < results.length; i++) {
-            question.options.push(results[i].data);
+        for (var i = 1; i < results.length; i++) {
+            question.required.push(results[i].data);
         }
 
         response.writeHeader(200, {"Content-Type": "application/json; charset=UTF-8"});
@@ -99,56 +91,109 @@ function generateResponse(nodeRequests, response) {
     });
 }
 
-http.createServer(function (request, response){
+function saveRelationshipsAndNotify (relationshipSaveTasks, response) {
+    async.series(relationshipSaveTasks, function(err, results) {
 
-    if (request.url != "/question") {
-        response.writeHeader(404, {"Content-Type": "text/plain"});
-        response.write("Not found");
-        response.end();
-        return;
-    }
+        if (err) {
+            response.writeHeader(500, {"Content-Type": "text/plain"});
+            response.write("Internal server error");
+            response.end();
+        } else {
+            response.writeHeader(200, {"Content-Type": "text/plain"});
+            response.write("Ok");
+            response.end();
+        }
 
+    });
+}
+
+function loadNodesAndAnswer (nodeRequests, response) {
+    async.series(nodeRequests, function(err, results) {
+
+        if (err) {
+            response.writeHeader(500, {"Content-Type": "text/plain"});
+            response.write("Internal server error");
+            response.end();
+            return;
+        }
+
+        var i;
+
+        /*
+         save relationships
+         */
+        var relationshipSaveTasks = new Array();
+        for (i = 1; i < results.length; i++) {
+            relationshipSaveTasks.push(createSaveRelationshipTask(results[0], results[i]));
+        }
+        saveRelationshipsAndNotify(relationshipSaveTasks, response);
+
+    });
+}
+
+function processQuestionRequest (request, response) {
     getNodeCount(function(nodeCount) {
 
         var i;
 
-        var numOfOptions = Math.min(nodeCount - 1, 4);
-
-        // choose node index 1
-        var masterNodeIndex = getRndNodeIndex(nodeCount);
-
         /*
-         choose node indices 2..(numOfOptions + 1)
+         create array of node indices
          */
         var nodeIndices = new Array();
-
-        nodeIndices.push(masterNodeIndex);
-
+        var numOfOptions = Math.min(nodeCount - 1, 4);
+        nodeIndices.push(getRndNodeIndex(nodeCount));
         for (i = 0; i < numOfOptions; i++) {
-
             var slaveNodeIndex = getRndNodeIndex(nodeCount);
-
-            // eliminate repetitive indices
             if (nodeIndices.indexOf(slaveNodeIndex) != -1) {
                 continue;
             }
-
             nodeIndices.push(slaveNodeIndex);
-
         }
 
         /*
-         run async.series() to get nodes
+         generate question
          */
         var nodeRequests = new Array();
-
         for (i = 0; i < nodeIndices.length; i++) {
-            nodeRequests.push(createGetNodeTask(nodeIndices[i]));
+            nodeRequests.push(createGetNodeByIndexTask(nodeIndices[i]));
         }
-
-        generateResponse(nodeRequests, response);
+        loadNodesAndQuestion(nodeRequests, response);
 
     });
+}
+
+function submitAnswer(request, response) {
+
+    var i;
+
+    var query = url.parse(request.url, true).query;
+    var targetNodeId = query["target"];
+    var requiredNodeIds = query["required"].split(" ");
+
+    /*
+     submit answer
+     */
+    var nodeGetTasks = new Array();
+    nodeGetTasks.push(createGetNodeByIdTask(targetNodeId));
+    for (i = 0; i < requiredNodeIds.length; i++) {
+        var nodeId = parseInt(requiredNodeIds[i]);
+        nodeGetTasks.push(createGetNodeByIdTask(nodeId));
+    }
+    loadNodesAndAnswer(nodeGetTasks, response);
+
+}
+
+http.createServer(function (request, response){
+
+    if (request.url == "/getQuestion") {
+        processQuestionRequest(request, response);
+    } else if (request.url.indexOf("/submitAnswer?") == 0) {
+        submitAnswer(request, response);
+    } else {
+        response.writeHeader(404, {"Content-Type": "text/plain"});
+        response.write("Not found");
+        response.end();
+    }
 
 }).listen(80);
 
